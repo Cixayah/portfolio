d902fee76eceba450ef6a20e034b47b11a842924
Gabriel "Cix" Costa <gabrielviniciodacosta@gmail.com>
2024-06-05 10:44:33 -0300

    #

--- src/components/Home/AboutMe.tsx
+++ src/components/Home/AboutMe.tsx
@@ -37,8 +37,6 @@ export const AboutMe = ({ aboutMe }: AboutMeProps) => {
                             {tech}
                         </li>
                     ))}
-
-
                 </ul>
             </div>
             <div className='relative'>
